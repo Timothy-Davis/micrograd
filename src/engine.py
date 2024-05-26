d469530d781a13450909fdc74676c5d73c5b6caa
timothy-davis <dtimmy690@gmail.com>
2024-05-26 18:23:26 -0500

    implemented back-propagation supporting only addition and multiplication (for now ;))

--- src/engine.py
+++ src/engine.py
@@ -5,6 +5,7 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
 
@@ -12,13 +13,46 @@ class Value:
         return self.data
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1 * out.grad
+            other.grad += 1 * out.grad
+
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
-        # out.grad += other.grad
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
         return out
 
     def tanh(self):
-        out = Value((math.exp(2 * self.data) - 1) / ((math.exp(2 * self.data)) + 1), (self,), 'tanh')
+        tanh = (math.exp(2 * self.data) - 1) / ((math.exp(2 * self.data)) + 1)
+        out = Value(tanh, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - tanh**2) * out.grad
+
+        out._backward = _backward
         return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1
+        for node in reversed(topo):
+            node._backward()
--- src/nn.py
+++ src/nn.py
@@ -1,3 +1,4 @@
+from graphviz import Digraph
 from random import random
 import engine
 
@@ -5,3 +6,6 @@ if __name__ == '__main__':
     a = engine.Value(random())
     b = engine.Value(random())
     c = engine.Value(random())
+    d = a * b + c
+    d = d.tanh()
+    d.backward()
