9e3fa51c7a2d7852b2f2146c06146f2467ccb381
timothy-davis <dtimmy690@gmail.com>
2024-05-26 22:25:06 -0500

    Added all other operations and replaced tanh non-linearity with reLU

--- src/engine.py
+++ src/engine.py
@@ -10,9 +10,10 @@ class Value:
         self._op = _op
 
     def __repr__(self):
-        return self.data
+        return f"{self.data}"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -23,6 +24,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -32,12 +34,42 @@ class Value:
         out._backward = _backward
         return out
 
-    def tanh(self):
-        tanh = (math.exp(2 * self.data) - 1) / ((math.exp(2 * self.data)) + 1)
-        out = Value(tanh, (self,), 'tanh')
+    def __rmul__(self, other):
+        return self * other
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only supports int and float"
+        out = Value(self.data ** other, (self, other), f'**{other}')
 
         def _backward():
-            self.grad += (1 - tanh**2) * out.grad
+            self.grad += ((self.data * other) ** (other - 1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __neg__(self):
+        return self * -1
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), "relu")
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
 
         out._backward = _backward
         return out
@@ -45,12 +77,14 @@ class Value:
     def backward(self):
         topo = []
         visited = set()
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
+
         build_topo(self)
 
         self.grad = 1
--- src/nn.py
+++ src/nn.py
@@ -3,9 +3,16 @@ from random import random
 import engine
 
 if __name__ == '__main__':
-    a = engine.Value(random())
-    b = engine.Value(random())
-    c = engine.Value(random())
-    d = a * b + c
-    d = d.tanh()
-    d.backward()
+    a = engine.Value(2.0)
+    b = engine.Value(0.0)
+    c = engine.Value(-3.0)
+    d = engine.Value(1.0)
+    e = engine.Value(6.8813735870195432)
+    f = a*c + b*d + e
+    g = f.relu()
+    print(g, f, e, d, c, b, a)
+    g.backward()
+    print(a.grad)
+    print(b.grad)
+    print(c.grad)
+    print(d.grad)
